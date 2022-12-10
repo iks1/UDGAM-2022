19524001ed05af3295e5c0fe8da14d92d0a04df4
Anurag <anurag.k.2180@gmail.com>
2022-12-11 00:01:08 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 
 import Home from './pages/Home';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Schedule from './pages/schedule';
 
 import Contactus from './pages/contactus';
 function App() {
   return (
     <>
-    <Home/>
-    <Schedule/>
-    <Contactus/>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+          {/* <Route index element={<Home />} /> */}
+          <Route exact path="/contactus" element={<Contactus />} />
+        </Route>
+      </Routes>
+    </Router>
+    
     </>
   );
 }
