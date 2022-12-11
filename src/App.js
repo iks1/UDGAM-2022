e4af624660299533c41a73dcea690dc17f4698a1
PriyanshSS <priyanshs18171819@gmail.com>
2022-12-11 17:36:15 +0530

    registration page done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@
 import Home from './pages/Home';
 
 import Schedule from './pages/schedule';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 import Contactus from './pages/contactus';
+import Registration from './pages/Registration';
+
 function App() {
   return (
     <>
-    <Home/>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/registration' element={<Registration/>}/>
+    </Routes>
+    {/* <Home/>
     <Schedule/>
-    <Contactus/>
+    <Contactus/> */}
+    
     </>
   );
 }
