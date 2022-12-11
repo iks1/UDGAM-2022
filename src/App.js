55c3a350c41a8226505fc0d7b9cc1e770741995d
iks1 <94672267+iks1@users.noreply.github.com>
2022-12-11 19:32:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Schedule from './pages/schedule';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 import Contactus from './pages/contactus';
@@ -11,17 +10,14 @@ import Registration from './pages/Registration';
 function App() {
   return (
     <>
-
+    <Router>
     <Routes>
       <Route exact path='/' element={<Home/>}/>
       <Route exact path='/registration' element={<Registration/>}/>
       <Route exact path="/contactus" element={<Contactus />} />
     </Routes>
-    {/* <Home/>
-    <Schedule/>
-    <Contactus/> */}
 
-    
+    </Router>
     
     </>
   );
