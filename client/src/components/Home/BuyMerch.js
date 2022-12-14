f644c2f537c80694307a6c2f335461e1a7b8c70a
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2022-12-14 10:29:36 +0530

    make responsive success failure

--- client/src/components/Home/BuyMerch.js
+++ client/src/components/Home/BuyMerch.js
@@ -8,7 +8,7 @@ function BuyMerch() {
     <div className='merch' id='merch'  style={{backgroundColor:"#0c0227"}}>
        <div class="top">
     <h1>BUY OUR MERCH</h1>
-    <button >View All Merch<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <button className='topbut' >View All Merch<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_27_4652)">
       <path d="M19 19.6367H5V5.63672H12V3.63672H5C3.89 3.63672 3 4.53672 3 5.63672V19.6367C3 20.7367 3.89 21.6367 5 21.6367H19C20.1 21.6367 21 20.7367 21 19.6367V12.6367H19V19.6367ZM14 3.63672V5.63672H17.59L7.76 15.4667L9.17 16.8767L19 7.04672V10.6367H21V3.63672H14Z" fill="#D4F34A"/>
       </g>
