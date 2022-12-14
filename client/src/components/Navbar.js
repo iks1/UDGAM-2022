8349ea31ae9096186b2dde4c9737d516d12c64ca
Your Name <you@example.com>
2022-12-14 12:37:55 +0530

    AAA

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Home/navbar_landing.css";
 import Icon from "../icons/icon.png"
 import Buy from "../icons/Ticket.png"
 import Hum from "../icons/hamburger.png"
-
+import Hamburger from './hamburger';
+import Humcross from "../icons/Cross Button.svg"
 function Navbar() {
-
+  useEffect(() => {
+    document.getElementById("open_nav").style.display="block";
+    document.getElementById("close_nav").style.display="none";
+  });
+function openNav() {
+  console.log("CLICKED")
+  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("open_nav").style.display="none";
+  document.getElementById("bignav1").style.display="none";
+  document.getElementById("bignav2").style.display="none";
+  document.getElementById("bignav3").style.display="none";
+  document.getElementById("close_nav").style.display="block";
+}
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("open_nav").style.display="block";
+  document.getElementById("close_nav").style.display="none";
+  document.getElementById("bignav1").style.display="block";
+  document.getElementById("bignav2").style.display="block";
+  document.getElementById("bignav3").style.display="block";
+}
   return (
+    <>
     <div className='header'>
       <section className='navbar'>
         <div className='image-container'>
@@ -15,12 +37,13 @@ function Navbar() {
 
         <div className='second-container'>
             <ul className='nav-links'>
-              <li className='nav-item'><a href='#container'>Events</a></li>
-              <li className='nav-item'><a href='#'>Schedule</a></li>
-              <li className='nav-item'><a href='#merch'>Merch</a></li>
+              <li className='nav-item' id="bignav1"><a href='#container'>Events</a></li>
+              <li className='nav-item'  id="bignav2"><a href='#'>Schedule</a></li>
+              <li className='nav-item'  id="bignav3"><a href='#merch'>Merch</a></li>
             </ul>
           <div className='hamburgerParent' >
-            <img className='hamburger' src={Hum} alt='menu' />
+            <a onClick={openNav} id="open_nav"><img className='hamburger'  src={Hum} alt='menu' /></a>
+            <a onClick={closeNav} id="close_nav"><img className='hamburger' src={Humcross} alt='menu' /></a>
           </div>
 
           <div className='yellowButtonParent' >
@@ -38,6 +61,12 @@ function Navbar() {
       </section>
 
     </div>
+<div id="myNav" className="overlay">
+  <Hamburger/>
+</div>
+
+<span >open</span>
+    </>
   )
 }
 
